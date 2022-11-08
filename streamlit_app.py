ad8b9372690ffea59a412abbe46765d98b34253b
Xanomon <117601481+Xanomon@users.noreply.github.com>
2022-11-09 10:10:46 +1300

    Add formatting.

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,3 +27,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# normalize fruityvice JSON response.
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# put the normalized data into a dataframe to display in a table.
+streamlit.dataframe(fruityvice_normalized)
